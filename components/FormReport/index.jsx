acba7efe0d792263c4f98ee62b69b20b23d3c0d4
MatheusPatriota <mpatriota55@gmail.com>
2021-08-18 00:52:55 -0300

    confirmacao de ocorrencia adicionada

--- components/FormReport/index.jsx
+++ components/FormReport/index.jsx
@@ -5,6 +5,7 @@ import { Button } from '../Button';
 import { ReportStyles } from './styles';
 import firebase from '../../lib/firebase';
 import { useRouter } from 'next/router';
+import swal from 'sweetalert';
 
 export function FormReport(props) {
   const [reportTitle, setReportTitle] = useState();
@@ -14,6 +15,7 @@ export function FormReport(props) {
   const { uid } = router.query;
   // console.log('usuario pagina de from',user, uid)
   // console.log(props.avatar)
+
   async function handleSendNewReport(event) {
     //gerando o timestamp da data atual
     let currentData = new Date().getTime() / 1000;
@@ -40,8 +42,30 @@ export function FormReport(props) {
     };
     await firebase.database().ref(`rooms/${uid}/reports`).push(report);
 
-    setReportText();
-    setReportTitle();
+    setReportText('');
+    setReportTitle('');
+
+    fetch('api/sendEmail', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: user.name,
+        email: user.email,
+        message: 'Sua Ocorrência foi cadastrada com sucesso!',
+      }),
+    })
+      .then((response) => {
+        swal(
+          'Ocorrencia cadastrada com Sucesso',
+          'Enviamos um email confirmando Sua nova Ocorrência =D',
+          'success',
+        );
+      })
+      .catch((err) => {
+        swal('Erro ao enviar Email', `${err.message}`, 'error');
+      });
   }
 
   return (
@@ -51,7 +75,7 @@ export function FormReport(props) {
           <h1>{props.roomTitle}</h1>
           <span>({props.qtdPerguntas}) Ocorrência(s)</span>
         </div>
-        <form onSubmit={handleSendNewReport}>
+        <form onSubmit={handleSendNewReport} id="form">
           <input
             type="text"
             placeholder="Titulo da Ocorrência"
@@ -59,6 +83,7 @@ export function FormReport(props) {
             onChange={(event) => {
               setReportTitle(event.target.value);
             }}
+            value={reportTitle}
           />
           <textarea
             name="Descrição da ocorrência"
@@ -66,13 +91,14 @@ export function FormReport(props) {
             onChange={(event) => {
               setReportText(event.target.value);
             }}
+            value={reportText}
           />
           <div className="formFooter">
             {/* <Button className="btnVideo">Anexar seu Vídeo</Button> */}
             <div className="userInfo">
               <div className="containerIcone">
                 {props.avatar ? (
-                  <img src={props.avatar} alt="icone autor"  className="icon"/>
+                  <img src={props.avatar} alt="icone autor" className="icon" />
                 ) : (
                   <BiUser className="icon" />
                 )}
--- components/ToggleTheme/index.tsx
+++ components/ToggleTheme/index.tsx
@@ -1,16 +1,19 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { ToggleThemeStyles } from './styles';
 
 export default function ToggleTheme() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <>
-      {colorMode === 'light' ? (
-        <MoonIcon w={6} h={6} onClick={toggleColorMode} />
-      ) : (
-        <SunIcon w={6} h={6} onClick={toggleColorMode} />
-      )}
-    </>
+    <div title="Mudar Tema">
+      <ToggleThemeStyles>
+        {colorMode === 'light' ? (
+          <MoonIcon w={6} h={6} onClick={toggleColorMode} />
+        ) : (
+          <SunIcon w={6} h={6} onClick={toggleColorMode} />
+        )}
+      </ToggleThemeStyles>
+    </div>
   );
 }
